18f801b8847191c624e33196df3d0b0596f098b0
Annihilat0r <pierre-henri.debris@orange.fr>
2017-03-18 15:24:40 +0100

    modify prod

--- build-api.sh
+++ build-api.sh
@@ -1,10 +1,9 @@
 #!/usr/bin/env bash
 
-docker-compose down
+sudo docker-compose down
 echo "Build..."
-docker build -t devops/bank-api -f Dockerfile.api .
-#cp docker-compose.api.yml docker-compose.yml
+sudo docker build -t devops/bank-api -f Dockerfile.api .
 echo "Compose..."
-docker-compose -f docker-compose.api.yml up -d
+sudo docker-compose -f docker-compose.api.yml up -d
 echo "Api launch"
-#rm docker-compose.yml
+
