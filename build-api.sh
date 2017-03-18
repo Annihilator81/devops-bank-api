8e415f8f7c888ca967ce695bc57e32d1c86516d6
croic <croicx2@hotmail.fr>
2017-03-18 15:21:50 +0100

    sudo commit

--- build-api.sh
+++ build-api.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
-docker-compose down
+sudo docker-compose down
 echo "Build..."
-docker build -t devops/bank-api -f Dockerfile.api .
+sudo docker build -t devops/bank-api -f Dockerfile.api .
 #cp docker-compose.api.yml docker-compose.yml
 echo "Compose..."
-docker-compose -f docker-compose.api.yml up -d
+sudo docker-compose -f docker-compose.api.yml up -d
 echo "Api launch"
 #rm docker-compose.yml
